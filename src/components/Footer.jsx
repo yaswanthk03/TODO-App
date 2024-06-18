f859b305cd15c032c5ce12fadf0e89d107bdf9a6
yaswanth kumar <201208@iiitt.ac.in>
2024-06-18 18:56:27 +0530

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+let Footer = () => {
+  return (
+    <footer className="bg-[#172842]">
+      <p className="text-white text-center">
+        &copy; {new Date().getFullYear()} Yaswanth Kumar
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
